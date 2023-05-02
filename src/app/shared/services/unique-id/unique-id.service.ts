a4dfb3f43c9f0425d34ec1c91dd4d18377e3533b
Ingrid Anjos <ingrid_anjos@hotmail.com>
2023-05-02 08:31:49 -0300

    feat: refatoracao da service

--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -3,10 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 
 @Injectable()
 export class UniqueIdService {
+   
     private numberOfGeneratedIds = 0;
+    private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
     public generatedUniqueIdWithPrefix(prefix: string): string {
-        if (!prefix) {
+        if (!prefix || !this.validId.test(prefix)) {
             throw Error('Prefix can not be empty')
         }
         const uniqueId = this.generateUniqueId();
